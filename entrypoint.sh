0797a707460e67da4932b3acb70d0b531f573f8d
Varun <varunsridharan23@gmail.com>
2020-04-29 12:05:56 +0530

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -eu
+echo " "
 echo "⬆️Envato Upload Started"
-cd $3;
-echo "##[group]Copying Files From $3";
+cd $3
+echo "##[group]Copying Files From $3"
 lftp "ftp.marketplace.envato.com" -u $1,$2 -e "set ftp:ssl-allow yes; mirror -R ./ ./; quit"
 ls -lah
 echo "##[endgroup]"
