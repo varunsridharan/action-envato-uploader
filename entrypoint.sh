800a5d0f37be345e347dba171a5386cd74c68695
Varun <varunsridharan23@gmail.com>
2020-10-22 09:10:47 +0530

    Updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,20 @@ ENVATO_USERNAME=$(gh_input "ENVATO_USERNAME")
 ENVATO_PERSONAL_TOKEN=$(gh_input "ENVATO_PERSONAL_TOKEN")
 ENVATO_DIST_DIR=$(gh_input "ENVATO_DIST_DIR" "./dist")
 
+gh_log " "
 gh_log "⬆️ Envato Upload Started"
 
 cd $ENVATO_DIST_DIR
 
+gh_log " "
 gh_log_group_start  "💿 Files To Be Copied From : ${ENVATO_DIST_DIR}"
 ls -lah
 gh_log_group_end
 
+gh_log " "
 gh_log_group_start  "⌛ Copying To FTP"
 lftp "ftp.marketplace.envato.com" -u $ENVATO_USERNAME,$ENVATO_PERSONAL_TOKEN -e "set ftp:ssl-allow yes; mirror -R ./ ./; quit"
 gh_log_group_end
 
+gh_log " "
 gh_log "👌 FTP Upload Complete"
